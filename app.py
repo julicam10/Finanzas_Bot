772ed6b72717db10c52f5c83adade358c2a041f2
Julian Camilo Espitia Caicedo <Julian@MacBook-Angie-Duarte.local>
2026-07-31 09:59:50 -0500

    Actualización en la tabla de presupuestos, se incluyo el campo del checkbox v7

--- app.py
+++ app.py
@@ -439,8 +439,8 @@ with pestana_presupuestos:
                 ).properties(height=250)
                 
                 st.altair_chart(grafico_tipo, use_container_width=True)
-            else:
-            st.info("No hay presupuestos configurados todavía.")
+    else:
+        st.info("No hay presupuestos configurados todavía.")
 
 with pestana_deudas:
     st.subheader("Control y Registro de Deudas")
